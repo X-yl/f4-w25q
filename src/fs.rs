48920a3370f9cb8538146c00d9594f01153b5387
x-yl <kyle@kylepereira.me>
2023-06-27 21:04:53 +0100

    Extract BLOCK_COUNT to user trait

--- src/fs.rs
+++ src/fs.rs
@@ -3,17 +3,35 @@ use stm32f4xx_hal::qspi::QspiPins;
 
 use crate::w25q::{SectorAddress, W25Q};
 
-impl<PINS: QspiPins> Storage for W25Q<PINS> {
+pub trait FSInfo {
+    const BLOCK_COUNT: usize;
+}
+
+pub struct W25QWrapper<INFO: FSInfo, PINS: QspiPins> {
+    flash: W25Q<PINS>,
+    _phantom: core::marker::PhantomData<INFO>,
+}
+
+impl<INFO: FSInfo, PINS: QspiPins> W25QWrapper<INFO, PINS> {
+    pub fn new(flash: W25Q<PINS>) -> Self {
+        Self {
+            flash,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<INFO: FSInfo, PINS: QspiPins> Storage for W25QWrapper<INFO, PINS> {
+    const BLOCK_COUNT: usize = INFO::BLOCK_COUNT;
     const READ_SIZE: usize = 256;
     const WRITE_SIZE: usize = 256;
     const BLOCK_SIZE: usize = 4096;
-    const BLOCK_COUNT: usize = 4096;
 
     type CACHE_SIZE = typenum::U512;
     type LOOKAHEAD_SIZE = typenum::U256;
 
     fn read(&mut self, off: usize, buf: &mut [u8]) -> littlefs2::io::Result<usize> {
-        self.read((off as u32).into(), buf)
+        self.flash.read((off as u32).into(), buf)
             .map_err(|_| {
                 littlefs2::io::Error::Io
             })
@@ -22,11 +40,11 @@ impl<PINS: QspiPins> Storage for W25Q<PINS> {
 
     fn write(&mut self, off: usize, data: &[u8]) -> littlefs2::io::Result<usize> {
         for (i, page) in data.chunks_exact(256).enumerate() {
-            self.program_page(((off + i * 256) as u32).into(), page)
+            self.flash.program_page(((off + i * 256) as u32).into(), page)
                 .map_err(|_| {
                     littlefs2::io::Error::Io
                 })?;
-            self.wait_on_busy().unwrap();
+            self.flash.wait_on_busy().unwrap();
         }
 
         Ok(data.len())
@@ -34,7 +52,7 @@ impl<PINS: QspiPins> Storage for W25Q<PINS> {
 
     fn erase(&mut self, off: usize, len: usize) -> littlefs2::io::Result<usize> {
         for i in 0..(len / 4096) {
-            self.erase_sector(SectorAddress::from_address((off + i * 4096) as u32))
+            self.flash.erase_sector(SectorAddress::from_address((off + i * 4096) as u32))
                 .map_err(|_| {
                     littlefs2::io::Error::Io
                 })?;
